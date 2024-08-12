542e5bf010fdb223d29db77e1fc855353090ddce
kartikmishra2004 <kartik26nov2004@gmail.com>
2024-08-12 20:58:20 +0530

    .

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -78,7 +78,7 @@ const Contact = () => {
                         </div>
 
                         <div className="fcf-form-group">
-                            <button type="submit" id="fcf-button" className="fcf-btn fcf-btn-primary fcf-btn-lg fcf-btn-block">{isLoading ? (<img src={loading} className='loader'></img>) : send}</button>
+                            <button type="submit" id="fcf-button" className="fcf-btn fcf-btn-primary fcf-btn-lg fcf-btn-block">{isLoading ? (<img src={loading} className='loader-img'></img>) : send}</button>
                         </div>
                     </form>
                 </div>
